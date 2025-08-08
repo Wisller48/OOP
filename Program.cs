04eb2aff7793f5728634877a7a2a85beb4b461a2
Brayan Wisller <126477343+Wisller48@users.noreply.github.com>
2025-08-08 09:52:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Pessoa{
+    //Atributos
+    public string nome = "";
+    public int idade = 0;
+
+    // Metodo
+    public void Apresentar(){
+        Console.WriteLine("Bom dia, "+nome+"! Você tem "+idade+" anos");
+    }
+}
+
+class Program{
+    public static void Main(string[] args){
+        Pessoa pessoa_1 = new Pessoa();
+        pessoa_1.nome = "Alex";
+        pessoa_1.idade = 15;
+
+        pessoa_1.Apresentar();
+
+        Pessoa pessoa_2 = new Pessoa();
+        pessoa_2.nome = "Maria";
+        pessoa_2.idade = 57;
+
+        pessoa_2.Apresentar();
+
+    }
+}
