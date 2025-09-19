11212c2ecd5fb397ac3ff737982d51a3556edf57
Brayan Wisller <126477343+Wisller48@users.noreply.github.com>
2025-09-19 10:43:44 -0300

    DataBase for Students

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,27 +1,46 @@
-﻿class Pessoa{
-    //Atributos
-    public string nome = "";
-    public int idade = 0;
+﻿using Microsoft.EntityFrameworkCore;
 
-    // Metodo
-    public void Apresentar(){
-        Console.WriteLine("Bom dia, "+nome+"! Você tem "+idade+" anos");
-    }
+using Escola.Models;
+using Escola.Data;
+
+
+Console.WriteLine("--- Escola DB ---");
+Console.WriteLine("Inicializando/Migrando o banco...");
+
+var db = new AppDBContext();
+
+await db.Database.MigrateAsync();
+
+var name = "Léo";
+var email = "leonardo@gmail.com";
+
+var exists = await db.Students.AnyAsync(s=> s.Email == email);
+
+if(exists){
+    Console.WriteLine("Já existe um usuário com esse email");
+    return;
 }
 
-class Program{
-    public static void Main(string[] args){
-        Pessoa pessoa_1 = new Pessoa();
-        pessoa_1.nome = "Alex";
-        pessoa_1.idade = 15;
+var Student = new Student{
+    Name = name,
+    Email = email,
+    EnrollnextDate = DateTime.UtcNow
+};
 
-        pessoa_1.Apresentar();
+db.Students.Add(Student);
+await db.SaveChangesAsync();
 
-        Pessoa pessoa_2 = new Pessoa();
-        pessoa_2.nome = "Maria";
-        pessoa_2.idade = 57;
+Console.WriteLine($"Cadastro com sucesso! Id: {Student.Id}");
 
-        pessoa_2.Apresentar();
+var lista_estudantes = await db.Students
+    .OrderBy(s=> s.Id)
+    .ToListAsync();
 
-    }
+if(lista_estudantes.Count == 0){
+    Console.WriteLine("Não há estudantes matriculados");
+    return;
+}
+
+foreach(var s in lista_estudantes){
+    Console.WriteLine($"{s.Id, 2} | {s.Name, -20} | {s.Email, -24} | {s.EnrollnextDate:yyyy-MM-dd HH:mm:ss}");
 }
